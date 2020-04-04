4a5c1b064932cc100e783c3463c8009de1490c02
George Markhulia <george.markhulia@volvocars.com>
2020-04-04 23:59:27 +0200

    Fix database relationships

--- hlpr/services/db/models.py
+++ hlpr/services/db/models.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, Float
 import datetime
-
 from sqlalchemy import ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -24,8 +23,8 @@ class User(BASE):
 class Helper(User):
     __tablename__ = "helper"
     id = Column(Integer, primary_key=True)
-    tasks = relationship("Service")
-    rating = relationship("Rating")
+    tasks = relationship("Service", backref="helper", lazy="select")
+    rating = relationship("Rating", backref="helper", lazy="select")
 
     def __str__(self):
         return f"User {self.name} {Helper.__tablename__}, rating {self.rating}"
@@ -34,33 +33,32 @@ class Helper(User):
 class Shopper(User):
     __tablename__ = "shopper"
     id = Column(Integer, primary_key=True)
-    tasks = relationship("Service")
+    tasks = relationship("Service", backref="shopper", lazy="select")
 
     def __str__(self):
         return f"User {self.name} {Shopper.__tablename__}"
 
 
-class Service:
-    __tablename__ = 'service'
+class Service(BASE):
+    __tablename__ = "service"
     id = Column(Integer, primary_key=True)
     task = Column(String(256), index=True)
-    shopperId = Column(Integer, ForeignKey("shopper.id"), nullable=False)
-    shopper = relationship("Shopper", back_populates="tasks")
-    helperId = Column(Integer, ForeignKey("helper.id"), nullable=False)
-    helper = relationship("Helper", back_populates="tasks")
+    shopperId = Column(Integer, ForeignKey("shopper.id"), nullable=True)
+    helperId = Column(Integer, ForeignKey("helper.id"), nullable=True)
 
     def __str__(self):
-        return f"Task: {self.task}, created by {self.shopper} is picked up by " \
-               f"{self.helper}"
+        return (
+            f"Task: {self.task}, created by {self.shopper} is picked up by "
+            f"{self.helper}"
+        )
 
 
-class Rating:
-    __tablename__ = 'ratings'
+class Rating(BASE):
+    __tablename__ = "ratings"
     id = Column(Integer, primary_key=True)
     rating = Column(Integer)
     comment = Column(String(512))
     helperId = Column(Integer, ForeignKey("helper.id"), nullable=False)
-    helper = relationship("Helper", back_populates="tasks")
 
 
 BASE.metadata.create_all(ENGINE)
