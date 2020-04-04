6a6b7c7bd0caff9e0c3b850bdddc803fc7251e81
Leonard Aukea <leonard.aukea@volvocars.com>
2020-04-04 16:57:28 +0200

    fixed serve func and added methodnames

--- backend/task_server.py
+++ backend/task_server.py
@@ -14,14 +14,31 @@ class TaskServicer(task_pb2_grpc.TaskServiceServicer):
     """Provides methods that implement functionality of task server."""
 
     def __init__(self):
+        pass 
 
+    def Create(self, request, context): 
+        pass
 
+    def Delete(self, request, context):
+        pass
+    
+    def Nearby(self, request, context):
+        pass
+
+    def Assign(self, request, context):
+        pass 
+
+    def SetStatus(self, request, context):
+        pass
+
+    def ListTags(self, request, context):
+        pass
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    route_guide_pb2_grpc.add_RouteGuideServicer_to_server(
-        RouteGuideServicer(), server)
+    task_pb2_grpc.add_TaskServiceServicer_to_server(
+        TaskServiceServicer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
--- backend/user_server.py
+++ backend/user_server.py
@@ -14,15 +14,33 @@ class UserServicer(user_pb2_grpc.UserServiceServicer):
 
 
     def __init__(self):
+        pass 
 
+    def Create(self, request, context): 
+        pass
 
+    def Delete(self, request, context):
+        pass
+    
+    def Nearby(self, request, context):
+        pass
 
+    def Reviews(self, request, context):
+        pass
 
+    def SubmitReview(self, request, context):
+        pass
+
+    def UpdatePoints(self, request, context): 
+        pass
+
+    def UserTags(self, request, context):
+        pass
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    route_guide_pb2_grpc.add_RouteGuideServicer_to_server(
-        RouteGuideServicer(), server)
+    user_pb2_grpc.add_UserServiceServicer_to_server(
+        UserServiceServicer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
