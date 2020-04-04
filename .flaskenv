6122c800d074d34910c6ea1d34a76707970c1c4b
Leonard Aukea <leonard.aukea@volvocars.com>
2020-04-04 23:17:20 +0200

    master cleanup

--- .flaskenv
+++
@@ -1,4 +0,0 @@
-SECRET_KEY='super-secure-key'
-FLASK_APP=hlpr.py
-FLASK_DEBUG=true
-MAIL_SERVER=localhost
--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from config import Config
-
-app = Flask(__name__)
-
-
-def create_app(config_class=Config):
-    pass
