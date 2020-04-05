8a093fb8efcc5e31ec70b9970ab7c2e1952294dd
Leonard Aukea <leonard.aukea@volvocars.com>
2020-04-05 12:22:05 +0200

    removed conn init we can use it later

--- hlpr/services/db/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Module that manages database connection."""
-import os
-from sqlalchemy_wrapper import SQLAlchemy
-
-
-# Connect to the database
-connection = SQLAlchemy(os.environ['DATABASE_URL'])
-
-
