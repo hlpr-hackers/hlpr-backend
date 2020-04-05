35e093c4133e427e5bfc570327d765463c520e44
George Markhulia <george.markhulia@volvocars.com>
2020-04-05 10:50:04 +0200

    Add tasks and shoppers mock objects

--- hlpr/services/db/mock.py
+++ hlpr/services/db/mock.py
@@ -1,10 +1,9 @@
 import datetime
 from sqlalchemy import create_engine
-import conn
-from models import User, Helper, Shopper, Task, Rating
+from models import Helper, Shopper, Task, Rating
 from sqlalchemy.orm import sessionmaker
 import sys
-
+import random
 
 def drop_all(eng):
     try:
@@ -16,39 +15,52 @@ def drop_all(eng):
         print(f"Failed to drop database {e}")
 
 
-def create_helper(session):
-    helper = Helper()
-    helper.name = "george"
-    helper.avg_rating = 4.7
-    helper.phone_number = "12345567"
-    helper.postCode = "41720"
-    helper.joined = datetime.datetime.now()
-    session.add(helper)
-    session.commit()
+def create_helper(session, helpers):
+    for name, attrib in helpers.items():
+        helper = Helper()
+        helper.name = name
+        helper.phone_number = attrib[0]
+        helper.avg_rating = attrib[1]
+        helper.postCode = attrib[2]
+        helper.joined = attrib[3]
+        session.add(helper)
+        session.commit()
 
 
-def create_shopper(session):
-    shopper = Shopper()
-    shopper.name = "granma3"
-    shopper.phone_number = "870231284"
-    shopper.postCode = "41720"
-    shopper.joined = datetime.datetime.now()
-    session.add(shopper)
-    session.commit()
+def create_shopper(session, shoppers):
+    for name, attrib in shoppers.items():
+        shopper = Shopper()
+        shopper.name = name
+        shopper.phone_number = attrib[0]
+        shopper.postCode = attrib[1]
+        shopper.joined = attrib[2]
+        session.add(shopper)
+        session.commit()
 
 
-def create_task(session, User):
-    shopper = session.query(Shopper).filter(Shopper.phone_number == "8702384").first()
-    task = Task()
-    task.name = "Take out garbage"
-    task.status = "open"
-    task.submision_timestamp = datetime.datetime.utcnow()
-    task.tag = "homehelp"
-    task.points = 3
-    task.shopperId = shopper.id
-    shopper.task = task
-    session.add_all([task, shopper])
-    session.commit()
+def create_task(session, tasks):
+    for task_name, attrib in tasks.items():
+        phone_num = attrib[4]
+        shopper = (
+            session.query(Shopper).filter(Shopper.phone_number == phone_num).first()
+        )
+        task = Task()
+        task.name = task_name
+        task.status = attrib[0]
+        task.submision_timestamp = attrib[1]
+        task.tag = attrib[2]
+        task.points = attrib[3]
+        task.shopperId = shopper.id
+        shopper.task = task
+        session.add_all([task, shopper])
+        session.commit()
+
+
+def assign_tasks(session, helpers):
+    tasks = session.query(Task).all()
+    for helper_num in helpers:
+        helper = session.query(Helper).filter(Helper.phone_number == helper_num).first()
+        task = tasks.pop(random.choice(tasks))
 
 
 if __name__ == "__main__":
@@ -74,23 +86,47 @@ if __name__ == "__main__":
         time_now - datetime.timedelta(6),
     ]
     helpers_collection = {
-        "helpers": helpers,
-        "helper_numbers": helper_numbers,
-        "helper_ratings": helper_ratings,
-        "helper_post_codes": helper_post_codes,
-        "helper_joined": helper_joined,
+        z[0]: list(z[1:])
+        for z in zip(
+            helpers, helper_numbers, helper_ratings, helper_post_codes, helper_joined
+        )
     }
 
     shoppers = ["granma", "granpa"]
-    shoppers_numbers = ['0738492', '0738491']
-    # shopper.phone_number = "870231284"
-    # shopper.postCode = "41720"
-    # shopper.joined = datetime.datetime.now()
-    # session.add(shopper)
+    shopper_numbers = ["0738492", "0738491"]
+    shooper_post_codes = ["41720", "41721"]
+    shopper_joined = [
+        time_now - datetime.timedelta(3),
+        time_now - datetime.timedelta(6),
+    ]
+
+    shoppers_collection = {
+        z[0]: list(z[1:])
+        for z in zip(shoppers, shopper_numbers, shooper_post_codes, shopper_joined)
+    }
+
+    tasks = ["Take out garbage", "Walk the dog"]
+    task_status = ["open", "open"]
+    task_submision_timestamp = [
+        time_now - datetime.timedelta(1),
+        time_now - datetime.timedelta(2),
+    ]
+    task_tags = ["homehelp", "pethelp"]
+    task_points = [4, 3]
+    requested_by = shopper_numbers
+
+    tasks_collection = {
+        z[0]: list(z[1:])
+        for z in zip(
+            tasks, task_status, task_submision_timestamp, task_tags, task_points, requested_by
+        )
+    }
 
     SESSION = sessionmaker(bind=engine)
     session = SESSION()
-    # create_helper(session)
-    # create_shopper(session)
-    # drop_all(engine)
-    create_task(session, Shopper)
+
+    # create_helper(session, helpers_collection)
+    # create_shopper(session, shoppers_collection)
+    # create_task(session, tasks_collection)
+    helpers = random.sample(helper_numbers, 2)
+    assign_tasks(session, helpers)
