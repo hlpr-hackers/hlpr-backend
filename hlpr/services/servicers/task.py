ee749bb9658982039717c9b8f5030b4605ecd656
George Markhulia <george.markhulia@volvocars.com>
2020-04-05 18:39:21 +0200

    Bugfix broken task.status

--- hlpr/services/servicers/task.py
+++ hlpr/services/servicers/task.py
@@ -6,8 +6,7 @@ import sys
 import grpc
 from sqlalchemy.orm import sessionmaker
 from hlpr.services.stubs import task_pb2, task_pb2_grpc
-from hlpr.services.db.models import Citizen, Helper, Task
-from hlpr.services.db import conn
+from hlpr.services.db import conn, models
 from sqlalchemy import create_engine
 import sqlalchemy.ext.declarative
 import datetime
@@ -29,16 +28,16 @@ class TaskServicer(task_pb2_grpc.TaskServiceServicer):
 
     def Create(self, request, context):
         session = self.session
-        citizen = session.query(Citizen).filter(
-            Citizen.phone_number == request.UUID
+        citizen = session.query(models.Citizen).filter(
+            models.Citizen.phone_number == request.UUID
         ).first()
-        tasks = session.query(Task).all()
-        task = Task()
+        tasks = session.query(models.Task).all()
+        task = models.Task()
         task.UUID = str(uuid.uuid1())
         task.name = request.name
         task.description = request.description
         task.points = request.points
-        task.status = "open"
+        task.status = "UNASSIGEND"
         task.submission_tstamp = datetime.datetime.utcnow()
         task.completed_tstamp = None
         task.tag = request.tag
@@ -46,14 +45,12 @@ class TaskServicer(task_pb2_grpc.TaskServiceServicer):
         session.add_all([task, citizen])
         session.commit()
         return task_pb2.Task(
-            id=task.UUID,
             name=task.name,
             status=task.status,
             description = task.description,
             tag = task.tag,
             points = task.points,
-            submission_tstamp = Timestamp().GetCurrentTime(),
-            completed_tstamp = Timestamp().GetCurrentTime()
+            submission_tstamp = Timestamp().GetCurrentTime()
         )
 
     def Delete(self, request, context):
